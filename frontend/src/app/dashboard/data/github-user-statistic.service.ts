e42dd40239c7d841dfdb6131a74091a802122de1
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2024-06-21 11:52:53 +0200

    clean up frontend dependencies

--- frontend/src/app/dashboard/data/github-user-statistic.service.ts
+++ frontend/src/app/dashboard/data/github-user-statistic.service.ts
@@ -7,13 +7,10 @@ import SockJS from 'sockjs-client';
 })
 export class GithubUserStatisticService {
   
-  private stompClient!: Stomp.Client;
-  
-  private serverUrl = 'http://localhost:8080/ws'; // Replace with your WebSocket endpoint
+  private readonly socket = new SockJS('http://localhost:8080/ws');
+  private readonly stompClient = Stomp.over(this.socket);
   
   connectToWebSocket() {
-    var socket = new SockJS(this.serverUrl); // Replace with your WebSocket endpoint
-    this.stompClient = Stomp.over(socket);
     this.stompClient.connect({}, (frame) => {
       console.log('Connected: ' + frame);
       this.stompClient.subscribe('/topic/github/user/updates', (message) => {
