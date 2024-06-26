84b3c46d2071b71718e857f337fbc30026cf6caf
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2024-06-26 18:31:16 +0200

    added environment.ts

--- frontend/src/app/dashboard/data/statistic/listener/github-user-statistic-listener.service.ts
+++ frontend/src/app/dashboard/data/statistic/listener/github-user-statistic-listener.service.ts
@@ -2,20 +2,21 @@ import { computed, Injectable, Signal, signal, WritableSignal } from '@angular/c
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 import { GithubUserStatistic } from '../github-user-statistic';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubUserStatisticListenerService {
   
-  private readonly socket = new SockJS('http://localhost:8080/ws');
+  private readonly socket = new SockJS(environment.localhostWS);
   private readonly stompClient = Stomp.over(this.socket);
   
   private readonly _githubUserStatistic: WritableSignal<GithubUserStatistic | undefined> = signal(undefined);
   
   constructor() {
     this.stompClient.connect({}, (frame) => {
-      this.stompClient.subscribe('/topic/github-user-statistic', (message) => {
+      this.stompClient.subscribe(environment.topicGithubUserStatistic, (message) => {
         try {
           const parsedMessage = JSON.parse(message.body) as GithubUserStatistic;
           this._githubUserStatistic.set(parsedMessage);
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment: any = {
+  localhostWS: 'http://localhost:8080/ws',
+  topicGithubUserStatistic: '/topic/github-user-statistic'
+};
