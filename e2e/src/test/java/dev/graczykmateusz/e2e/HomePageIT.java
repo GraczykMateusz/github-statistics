f383369ff20d9cfdd30ca7b2305a547a0e864bc4
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2024-08-03 13:46:27 +0200

    another try with selenium

--- e2e/src/test/java/dev/graczykmateusz/e2e/HomePageIT.java
+++ e2e/src/test/java/dev/graczykmateusz/e2e/HomePageIT.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 import javax.imageio.ImageIO;
@@ -16,6 +17,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -34,12 +37,14 @@ public class HomePageIT {
   }
 
   @BeforeEach
-  public void setup() {
+  public void setup() throws MalformedURLException {
     assertTrue(WebDriverManager.isDockerAvailable());
 
+    // Setup WebDriverManager for the FirefoxDriver
     WebDriverManager.firefoxdriver().setup();
 
-    driver = wdm.create();
+    String remoteUrl = "http://selenium:4444/wd/hub";
+    driver = new RemoteWebDriver(new URL(remoteUrl), new FirefoxOptions());
     driver.manage().window().maximize();
   }
 
